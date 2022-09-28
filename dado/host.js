e55e4f33b422a162f72427870dd35b22fc27d259
Lucas Baptista <Lucas.jesus.13>
2022-09-28 10:37:05 -0300

    28-09-2022

--- dado/host.js
+++ dado/host.js
@@ -1,12 +1,12 @@
 export default class Host {
     static url(){
-      return "https://gestaocomercial-git-dev-lucastw13.vercel.app/";
+      return "https://gestaocomercial-git-dev-lucastw13.vercel.app";
       return "http://lucas-pc:3000";
       return "http://localhost:3000";
     
     } 
     static urlApi(){
-        return "https://gestaocomercial-api-1hdc-2o9qdfb0b-lucastw13.vercel.app/";
+        return "https://gestaocomercial-api-1hdc-2o9qdfb0b-lucastw13.vercel.app";
         return "http://lucas-pc:3001";
         return "http://localhost:3001";
       
