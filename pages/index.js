a03a20cae878a26303d38770be6795e765631f6f
Lucas Baptista <Lucas.jesus.13>
2022-09-29 08:02:46 -0300

    host

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
 import Menu from './menu.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container} from 'reactstrap';
+import Host from '../dado/host';
 function Insumo() {
     
     return (
         <Container>
             <Menu />
             <br/><br/><h1>Gestão Comercial</h1>
+            <br/><br/><h1>{Host.url()}</h1>
+            <br/><br/><h1>{Host.urlApi()}</h1>
 
         </Container >
     );
