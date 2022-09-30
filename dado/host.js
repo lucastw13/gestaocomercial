bf013c7aa41fc9cc549dc0205ddf964e12db0798
Lucas Baptista <Lucas.jesus.13>
2022-09-30 16:43:08 -0300

    next.config

--- dado/host.js
+++ dado/host.js
@@ -1,10 +1,8 @@
 export default class Host {
   static url() {
-    console.log(process.env.URL)
     return process.env.URL
   }
   static urlApi() {
-    console.log(process.env.URL_API)
     return process.env.URL_API
   }
 }
---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    env: {
+        PORT: 3005,
+        URL: 'http://lucas-pc:3000',
+        URL_API: 'https://gestaocomercialapi.herokuapp.com'
+    },
+}
