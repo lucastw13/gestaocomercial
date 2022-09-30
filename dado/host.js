f0fb2fe78ac901999792cc47775f0fe3c31e2b2b
Lucas Baptista <Lucas.jesus.13>
2022-09-30 09:32:16 -0300

    Heroku

--- dado/host.js
+++ dado/host.js
@@ -1,12 +1,12 @@
 export default class Host {
     static url(){
-      //return "https://gestaocomercial-git-dev-lucastw13.vercel.app";
+      return "https://gestaocomercial1.herokuapp.com/";
       return "http://lucas-pc:3000";
       return "http://localhost:3000";
     
     } 
     static urlApi(){
-       //return "https://gestaocomercial-api-1hdc.vercel.app";
+       return "https://gestaocomercialapi.herokuapp.com/";
         return "http://lucas-pc:3001";
         return "http://localhost:3001";
       
