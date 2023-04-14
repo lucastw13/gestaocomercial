a66e627e1fdd3aef2b278cc6d1e47cbff91343ec
Lucas Baptista <Lucas.jesus.13>
2023-04-14 17:49:57 -0300

    2023-04-14 - 4

--- pages/compra.js
+++ pages/compra.js
@@ -1,11 +1,9 @@
 import { useState, React, useEffect } from 'react';
 import Menu from './menu.js';
-import { Container, Table, Form, FormGroup, Label, Input, Button, Modal, ModalHeader, ModalBody } from 'reactstrap';
+import { Container, Table, Form, Button, Modal, ModalHeader, ModalBody } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Dado from '../dado/generico.js';
 import Host from '../dado/host';
-import UtilDataURIToBlob from '../util/DataURIToBlob';
-import axios from 'axios';
 import Carregamento from './carregamento';
 import { useRouter } from 'next/router.js';
 import { QrReader } from 'react-qr-reader';
@@ -41,55 +39,11 @@ function Compra() {
             });
     }
 
-
-   
-    function importar() {
-        var file = document.getElementById("imagem").files[0];
-        var reader = new FileReader();
-
-        reader.onloadend = function () {
-            const blob = UtilDataURIToBlob.DataURIToBlob(reader.result)
-            const formData = new FormData();
-            formData.append('file', blob, 'image.jpg')
-            axios.post("https://api.qrserver.com/v1/read-qr-code/", formData)
-                .then(response => {
-                    if (response.data != null) {
-                        for (var item of response.data) {
-                            for (var itemSymbol of item.symbol) {
-                                console.log(itemSymbol)
-                                if (itemSymbol.data != null) {
-                                    var chave = itemSymbol.data.substring(itemSymbol.data.toUpperCase().indexOf("=") + 1, itemSymbol.data.toUpperCase().indexOf("|"))
-                                    if (chave != "") {
-                                        router.push(Host.url() + "/compraimportar/" + chave)
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }, (error) => {
-                    console.log("error: " + error)
-                })
-        }
-
-        if (file) {
-            reader.readAsDataURL(file);
-        } else {
-
-        }
-    }
-
     return (
         <Container>
             <Menu />
             <Form>
-                <FormGroup check inline>
-                    <Input type="file" id="imagem" />
-
-                </FormGroup>
-
-                <FormGroup check inline>
-                    <Button color="danger" onClick={toggleModal}>Importar</Button>
-                </FormGroup>
+                <Button color="danger" onClick={toggleModal}>Importar</Button>
             </Form>
             <Table>
                 <thead>
--- pages/insumodepara.js
+++ pages/insumodepara.js
@@ -1,18 +1,19 @@
 import { useState, React, useEffect } from 'react';
 import Menu from './menu.js';
-import { Button, Container, Table, FormGroup, Form, Label, Input } from 'reactstrap';
+import { Button, Container, Table, Form, Modal, ModalHeader, ModalBody  } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Dado from '../dado/generico.js';
 import Host from '../dado/host';
-import UtilDataURIToBlob from '../util/DataURIToBlob';
 import { useRouter } from 'next/router'
-import axios from 'axios'
+import { QrReader } from 'react-qr-reader';
 import Carregamento from './carregamento';
 function Insumodepara() {
     const [lista, setLista] = useState("");
     const [chave, setChave] = useState("");
     const router = useRouter()
     const [carregando, setCarregando] = useState("")
+    const [modal, setModal] = useState(false);
+    const toggleModal = () => setModal(!modal);
 
     useEffect(() => {
         listar()
@@ -58,53 +59,13 @@ function Insumodepara() {
 
     }
 
-    function importar() {
-        var file = document.getElementById("imagem").files[0];
-        var reader = new FileReader();
-
-        reader.onloadend = function () {
-            const blob = UtilDataURIToBlob.DataURIToBlob(reader.result)
-            const formData = new FormData();
-            formData.append('file', blob, 'image.jpg')
-            axios.post("http://api.qrserver.com/v1/read-qr-code/", formData)
-                .then(response => {
-                    if (response.data != null) {
-                        for (var item of response.data) {
-                            for (var itemSymbol of item.symbol) {
-                                console.log(itemSymbol)
-                                if (itemSymbol.data != null) {
-                                    var chave = itemSymbol.data.substring(itemSymbol.data.toUpperCase().indexOf("=") + 1, itemSymbol.data.toUpperCase().indexOf("|"))
-                                    if (chave != "") {
-                                        router.push(Host.url() + "/insumodeparaimportar/" + chave)
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }, (error) => {
-                    console.log("error: " + error)
-                })
-        }
-
-        if (file) {
-            reader.readAsDataURL(file);
-        } else {
-
-        }
-    }
+   
 
     return (
         <Container>
             <Menu />
             <Form>
-                <FormGroup check inline>
-                    <Input type="file" id="imagem" />
-
-                </FormGroup>
-
-                <FormGroup check inline>
-                    <Button color="danger" onClick={importar}>Importar</Button>
-                </FormGroup>
+                <Button color="danger" onClick={importar}>Importar</Button>
             </Form>
             <Table>
                 <thead>
@@ -144,6 +105,30 @@ function Insumodepara() {
             {carregando &&
                 <Carregamento/>
             }
+            <Modal isOpen={modal} toggle={toggleModal}>
+                <ModalHeader toggle={toggleModal}>Leitor QR</ModalHeader>
+                <ModalBody>
+                    <QrReader
+                        constraints={{
+                            facingMode: 'environment'
+                        }}
+                        onResult={(result, error) => {
+                            if (!!result) {
+                                var chave = result?.text
+                                chave = chave.substring(chave.toUpperCase().indexOf("=") + 1, chave.toUpperCase().indexOf("|"))
+                                if (chave != "") {
+                                    router.push(Host.url() + "/insumodeparaimportar/" + chave)
+                                }
+                            }
+
+                            if (!!error) {
+                                console.info(error);
+                            }
+                        }}
+                        style={{ width: '100%' }}
+                    />
+                </ModalBody>
+            </Modal>
         </Container>
     );
 
