1a8a87f83dd64cbc426de1f6729007fdedd10f96
Lucas Baptista <Lucas.jesus.13>
2024-05-05 19:41:33 -0300

    1

--- pages/compra.js
+++ pages/compra.js
@@ -17,6 +17,59 @@ function Compra() {
     const toggleModal = () => setModal(!modal);
     useEffect(() => {
         setInformacao("teste")
+        var url = "http://www4.fazenda.rj.gov.br/consultaNFCe/QRCode?p=33240431698759000970651220000303281967213329|2|1|1|1E8122A29B5E6A25BC7C4B637428BC7F5C6FC360"
+
+        fetch(url)
+            .then(function (response) {
+                switch (response.status) {
+                    // status "OK"
+                    case 200:
+                        return response.text();
+                    // status "Not Found"
+                    case 404:
+                        throw response;
+                }
+            })
+            .then(function (template) {
+                console.log(template);
+            })
+            .catch(function (response) {
+                // "Not Found"
+                console.log(response.statusText);
+            });
+
+        /*const options = {
+            uri: url
+        };
+        
+        requestPromise.get(options, (error, response) => {
+            console.log(response)
+            if (error) {
+                // Do error handling stuff
+            } else {
+                if (response.statusCode !== 200) {
+                     // Do error handling stuff
+                } else {
+                     // Do success stuff here
+                }
+            }
+        });*/
+        /*var httpRequest = new XMLHttpRequest();
+        httpRequest.open("GET", url, true);
+        console.log(httpRequest)
+        httpRequest.onreadystatechange = function () {
+            console.log("teste")
+            console.log(httpRequest)
+            if (httpRequest.readyState == 4) {
+                if (httpRequest.status == 200) {
+                    var xml = httpRequest.responseXML;
+                    console.log(getElementById("tabResult").getElementsByTagName("tbody")[0].getElementsByTagName("tr")[0].getElementsByTagName("td")[0].getElementsByTagName("span")[1].innerHTML.replace("\n","").replace("\t",""));
+                    var inf = getElementById("tabResult").getElementsByTagName("tbody")[0].getElementsByTagName("tr")[0].getElementsByTagName("td")[0].getElementsByTagName("span")[1].innerHTML.replace("\n", "").replace("\t", "")
+                    setInformacao(inf)
+                    toggleModal
+                }
+            }
+        }*/
         listar()
     }, [])
 
@@ -110,9 +163,9 @@ function Compra() {
                                         if (httpRequest.status == 200) {
                                             var xml = httpRequest.responseXML;
                                             //console.log(getElementById("tabResult").getElementsByTagName("tbody")[0].getElementsByTagName("tr")[0].getElementsByTagName("td")[0].getElementsByTagName("span")[1].innerHTML.replace("\n","").replace("\t",""));
-                                            var inf = getElementById("tabResult").getElementsByTagName("tbody")[0].getElementsByTagName("tr")[0].getElementsByTagName("td")[0].getElementsByTagName("span")[1].innerHTML.replace("\n","").replace("\t","")
-                                            setInformacao(inf)   
-                                            toggleModal 
+                                            var inf = getElementById("tabResult").getElementsByTagName("tbody")[0].getElementsByTagName("tr")[0].getElementsByTagName("td")[0].getElementsByTagName("span")[1].innerHTML.replace("\n", "").replace("\t", "")
+                                            setInformacao(inf)
+                                            toggleModal
                                         }
                                     }
                                 }
