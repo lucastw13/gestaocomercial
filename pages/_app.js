14d730dba643f8554dc94a4472b8b021a0ebf8b0
lucas.baptista.shift <lucas.baptista@shift.com.br>
2026-06-04 17:21:46 -0300

    deploy

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,7 @@
 import './estilo.css'
-//import { Inter} from '@next/font/google';
-//const inter = Inter ({ subsets: ['latin-ext'] });
 
 function MyApp({Component, pageProps}){
-    return <Component className={inter.className} {...pageProps}/>
+    return <Component {...pageProps}/>
 }
 
 export default MyApp
